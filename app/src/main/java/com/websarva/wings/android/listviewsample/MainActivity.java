5d00f9fc79b07f00057cf86563f38825bfbe0219
junnosuke789 <task.fuelex89@gmail.com>
2021-05-22 07:02:28 +0900

    書籍の模写終了

--- app/src/main/java/com/websarva/wings/android/listviewsample/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/listviewsample/MainActivity.java
@@ -3,6 +3,10 @@ package com.websarva.wings.android.listviewsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get Object.
+        ListView lvMenu = findViewById(R.id.lv_menu);
+        ListItemClicklistener listener = new ListItemClicklistener();
+
+        //Set listener
+        lvMenu.setOnItemClickListener(listener);
+    }
+
+    //Define listener class.
+    private class ListItemClicklistener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            //Get menu name which tapped.
+            String item = (String)parent.getItemAtPosition(position);
+            String show = "あなたが選んだ定食"+ item;
+
+            //Toast function.
+            Toast.makeText(MainActivity.this,show,Toast.LENGTH_LONG).show();
+
+
+        }
     }
 }
